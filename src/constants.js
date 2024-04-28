6afabaeba397d1e948644c64de8d3d162117ebe3
mendespedro <windowsxpedro@gmail.com>
2024-04-28 20:32:15 -0300

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const WHATS_APP_URL = "https://api.whatsapp.com/send/?phone=5548984012016"
+
+export const BOOK_URL = WHATS_APP_URL + "&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta"
